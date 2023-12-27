7dd873befdb2d1728d8b7fc4c6b3e676461b7fce
marciofao <marcio.lopes.fao@gmail.com>
2023-12-27 05:57:13 -0300

    implement error handling

--- routes/book.js
+++ routes/book.js
@@ -3,7 +3,24 @@ const { Router } = require('express')
 const router = Router()
 
 router.get('/', (req, res) => {
-    res.send('Hello World, it works!')
+    try{
+        res.send('Hello World, it works!')
+    }catch(error){
+        res.status(500)
+        res.send(error.message)
+    }
+})
+
+router.post('/', (req, res) => {
+    res.send('You sent a POST request')
+})
+
+router.patch('/', (req, res) => {
+    res.send('You sent a PATCH request')
+})
+
+router.delete('/', (req, res) => {
+    res.send('You sent a DELETE request')
 })
 
 module.exports = router
