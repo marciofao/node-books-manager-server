1c452a8ac8fd1e1eb77dd4deb72febc463f7b5eb
marciofao <marcio.lopes.fao@gmail.com>
2023-12-27 10:44:11 -0300

    fix merge

--- controllers/book.js
+++ controllers/book.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 function getBooks(req, res){
     try{
         //throw new Error('This is a custom error')
-=======
-function getBooks(req, res) => {
-    try{
->>>>>>> 797567f5191f6e5f099c25e1ef771ea1461911bf
         res.send('Hello World, it works!')
     }catch(error){
         res.status(500)
