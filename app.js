ca9d6b7e798b8e3a3eaac282fe18c92e5debe4e4
marciofao <marcio.lopes.fao@gmail.com>
2023-12-29 15:30:14 -0300

    isntall cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const routeBook = require('./routes/book')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+app.use(cors({origin: "*"}))
 
 app.use('/books', routeBook)
 
